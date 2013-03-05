cce2e3c3b298d83ed5689f0e5b421367b5d76590
ajudson <alanjudson@gmail.com>
2013-03-04 20:56:33 -0500

    new gemfile gem

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,18 @@ class PostsController < ApplicationController
   	@posts = Post.all
   end
 
+  def new
+  	@post = Post.new
+  end
 
+  def create
+  	@post = Post.new(params[:id])
+  	#if post is valid - go to index
+  	if @post.save
+  		redirect_to post_path
+  	else
+  		render :new
+  	end
+  end
 
 end
